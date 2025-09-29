31f8add588aabfeed8d54ae4f51971d94ac9b643
vl010101 <65632103+vl010101@users.noreply.github.com>
2025-09-29 15:51:12 +0100

    Update aceconfig.py

--- add/aceconfig.py
+++ add/aceconfig.py
@@ -77,7 +77,7 @@ class AceConfig(acedefconfig.AceDefConfig):
         '192.168.0.0/16',
         )
     # Maximum concurrent connections (video clients)
-    maxconns = 10
+    maxconns = 1000
     # Use 'Transfer-encoding: chunked' in HTTP AceProxy responses
     use_chunked = True
     #
